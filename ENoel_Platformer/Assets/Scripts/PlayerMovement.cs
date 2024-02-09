dc4f94b2470363aed233ea881ffcb6241f9aeb03
EvanNoelGames <evan.noel@mymail.champlain.edu>
2024-02-09 17:10:55 -0500

    Added crouch jumping

--- ENoel_Platformer/Assets/Scripts/PlayerMovement.cs
+++ ENoel_Platformer/Assets/Scripts/PlayerMovement.cs
@@ -82,15 +82,55 @@ public class PlayerMovement : MonoBehaviour
             canUncrouch = true;
         }
 
+        HandlePlayerMovement();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _isGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
+        Gizmos.DrawWireCube(new Vector3(transform.position.x - 0.2f, transform.position.y + 0.75f, transform.position.z), new Vector2(0.4f, 0.3f));
+        Gizmos.DrawWireCube(new Vector3(transform.position.x + 0.2f, transform.position.y + 0.75f, transform.position.z), new Vector2(0.4f, 0.3f));
+        Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y + 0.75f, transform.position.z), new Vector2(0.5f, 0.5f));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // if the player hits a wall while a special jump is occuring than stop them
+        if (!_isGrounded && xDesiredMovement != 0)
+        {
+            xDesiredMovement = 0;
+            xDampenMovement = 1;
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y / 2);
+        }
+
+        if (other.gameObject.CompareTag("Moving Platform"))
+        {
+            transform.SetParent(other.transform, true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Moving Platform"))
+        {
+            transform.SetParent(null, true);
+        }
+    }
+
+    private void HandlePlayerMovement()
+    {
         _rb.velocity = new Vector2((_xMoveInput * xDampenMovement) + xDesiredMovement, _rb.velocity.y);
 
         Crouching();
 
+        // if the player is grounded reset their movement modifiers
         if (_isGrounded)
         {
             xDesiredMovement = 0;
             xDampenMovement = 1;
         }
+        // otherwise count them down
         else
         {
             if (xDesiredMovement > 0)
@@ -113,52 +153,31 @@ public class PlayerMovement : MonoBehaviour
         {
             if (_isGrounded)
             {
-                _rb.AddForce(Vector2.up * jumpForce);
+                if (crouching)
+                {
+                    if (_rb.velocity.x != 0)
+                    {
+                        CrouchJump();
+                    }
+                    else if (canUncrouch)
+                    {
+                        _rb.AddForce(Vector2.up * jumpForce);
+                    }
+                }
+                else
+                {
+                    _rb.AddForce(Vector2.up * jumpForce);
+                }
             }
-            else if (canWallJumpRight)
+            // if the player is not on the ground (but attempting to jump) check if they can walljump
+            else
             {
-                _rb.velocity = Vector2.zero;
-                xDesiredMovement = 10.0f;
-                xDampenMovement = 0.1f;
-                _rb.AddForce(Vector2.up * jumpForce);
-            }
-            else if (canWallJumpLeft)
-            {
-                _rb.velocity = Vector2.zero;
-                xDesiredMovement = -10.0f;
-                xDampenMovement = 0.1f;
-                _rb.AddForce(Vector2.up * jumpForce);
+                WallJump();
             }
             _shouldJump = false;
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = _isGrounded ? Color.green : Color.red;
-        Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
-        Gizmos.DrawWireCube(new Vector3(transform.position.x - 0.2f, transform.position.y + 0.75f, transform.position.z), new Vector2(0.4f, 0.3f));
-        Gizmos.DrawWireCube(new Vector3(transform.position.x + 0.2f, transform.position.y + 0.75f, transform.position.z), new Vector2(0.4f, 0.3f));
-        Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y + 0.75f, transform.position.z), new Vector2(0.5f, 0.5f));
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Moving Platform"))
-        {
-            transform.SetParent(other.transform, true);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Moving Platform"))
-        {
-            transform.SetParent(null, true);
-        }
-    }
-
-    // get parent scale and multiply it maybe
     private void Crouching()
     {
         if (_shouldCrouch)
@@ -174,9 +193,53 @@ public class PlayerMovement : MonoBehaviour
             xSpeed = xStartingSpeed;
         }
 
-        // airbourne and crouching
+        // airborne and crouched
         if (!_isGrounded && crouching)
         {
+            transform.localScale = new Vector3(transform.localScale.x, 1.0f, transform.localScale.z);
+            crouching = false;
+            xSpeed = xStartingSpeed;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (canWallJumpRight)
+        {
+            _rb.velocity = Vector2.zero;
+            xDesiredMovement = 10.0f;
+            xDampenMovement = 0.1f;
+            _rb.AddForce(Vector2.up * jumpForce);
+        }
+        else if (canWallJumpLeft)
+        {
+            _rb.velocity = Vector2.zero;
+            xDesiredMovement = -10.0f;
+            xDampenMovement = 0.1f;
+            _rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void CrouchJump()
+    {
+        if (_rb.velocity.x > 0)
+        {
+            _rb.velocity = Vector2.zero;
+            xDesiredMovement = 20.0f;
+            xDampenMovement = 0.1f;
+            _rb.AddForce(Vector2.up * (jumpForce * 0.75f));
+
+            transform.localScale = new Vector3(transform.localScale.x, 1.0f, transform.localScale.z);
+            crouching = false;
+            xSpeed = xStartingSpeed;
+        }
+        else if (_rb.velocity.x < 0)
+        {
+            _rb.velocity = Vector2.zero;
+            xDesiredMovement = -20.0f;
+            xDampenMovement = 0.1f;
+            _rb.AddForce(Vector2.up * (jumpForce * 0.75f));
+
             transform.localScale = new Vector3(transform.localScale.x, 1.0f, transform.localScale.z);
             crouching = false;
             xSpeed = xStartingSpeed;
