cab5c2c3dbaeb60f7dd44d569c7e85627f7050f4
EvanNoelSchool <evan.noel@mymail.champlain.edu>
2024-02-11 18:26:19 -0500

    Added falling platforms

--- ENoel_Platformer/Assets/Scripts/CameraManager.cs
+++ ENoel_Platformer/Assets/Scripts/CameraManager.cs
@@ -6,13 +6,6 @@ public class CameraManager : MonoBehaviour
 {
     public Transform player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + new Vector3(0, 1, -5);
---
+++ ENoel_Platformer/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    public float timeBeforeRespawning = 6f;
+    public float timeBeforeFalling = 1f;
+    public float fallSpeed = 1f;
+
+    private bool falling = false;
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(0, 0);
+        _rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (falling)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, -fallSpeed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.transform.position.y > transform.position.y)
+            {
+                StartCoroutine(Co_Fall());
+            }
+        }
+    }
+
+    IEnumerator Co_Respawn()
+    {
+        yield return new WaitForSeconds(timeBeforeRespawning);
+        falling = false;
+        transform.position = startingPosition;
+        _rb.gravityScale = 0f;
+        _rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    IEnumerator Co_Fall()
+    {
+        yield return new WaitForSeconds(timeBeforeFalling);
+        falling = true;
+        _rb.gravityScale = 0.1f;
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        StartCoroutine(Co_Respawn());
+    }
+}
